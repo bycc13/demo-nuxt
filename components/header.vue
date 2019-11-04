ea98f3a922cb06599deebc8d63642cf6ed3264b6
bycc13 <bycc13@163.com>
2019-11-04 10:26:27 +0800

    first commit

---
+++ components/header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="">
+
+  </div>
+</template>
+
+<script>
+    export default {
+        name: '',
+        props: {},
+        data () {
+        return {
+
+        }
+        },
+        computed: {},
+        watch: {},
+        components: {
+
+        },
+        methods: {},
+        created () {},
+        mounted () {}
+    }
+</script>
+
+<style scoped lang="scss">
+
+ 
+</style>
---
+++ layouts/main.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="layout-main">
+        <header-main/>
+        <nuxt/>
+    </div>
+</template>
+
+<script>
+import headerMain from "@/components/header-main.vue";
+
+export default {
+    components: {
+        headerMain
+    }
+};
+</script>
+
+<style scoped>
+.layout-main {
+    padding-top: 70px;
+}
+</style>
